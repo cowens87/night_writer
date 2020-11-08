6d4518a39900678551fddc4ac63729385e082bf7
Cydnee Owens <cks410@yahoo.com>
2020-11-08 14:04:28 -0700

    update nr file until translator method is written

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -5,13 +5,13 @@ braille_message    = handle.read.chomp
 handle.close
 
 dictionary         = Dictionary.new
-text               = dictionary.braille_translator(braille_message)
-final_braille      = text.scan(/.{1,80}/)+"\n"
-require 'pry'; binding.pry
+# text               = dictionary.braille_translator(braille_message)
+# final_braille      = text.scan(/.{1,80}/)+"\n"
 
-braille_translator = dictionary.braille_translator(final_braille)
+# braille_translator = dictionary.braille_translator(final_braille)
 writer             = File.open(ARGV[1], "w")
-writer.write(braille_translator)
+writer.write(braille_message)
 writer.close
 
+# will need to count the translated file to get an accurate character count
 puts "Created #{ARGV[1]} containing #{braille_message.length} characters"
