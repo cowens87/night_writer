dc3aca3ad043d1f8f0a49ad43345cbc2d62054d5
Cydnee Owens <cks410@yahoo.com>
2020-11-08 08:11:52 -0700

    add additional testing

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -46,6 +46,8 @@ class DictionaryTest < Minitest::Test
 
   def test_it_can_translate_one_letter_to_braille
     assert_equal ['0.', '00', '..'], @dictionary.translate_letter('h')
+    assert_equal ["00", "..", "0."], @dictionary.translate_letter('m')
+    assert_equal ['0.', '00', '..'], @dictionary.translate_letter('u')
   end
 
   def test_it_can_separate_a_word
