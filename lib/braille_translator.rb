aa4d33a871291a938d2d91375cd8dbc5e298604b
Cydnee Owens <cks410@yahoo.com>
2020-11-09 12:39:26 -0700

    refactor join braille method

--- lib/braille_translator.rb
+++ lib/braille_translator.rb
@@ -22,14 +22,22 @@ class BrailleTranslator
   end
 
   def join_braille_characters(input)
-    braille_chars = []
+    # braille_chars = []
+    # positions = input.split("\n")
+    # num_of_letters(input).times do
+    #   positions.each do |position|
+    #     braille_chars << [position.slice!(0..1)]
+    #   end
+    # end
+    # braille_chars.join
+
     positions = input.split("\n")
     num_of_letters(input).times do
-      positions.each do |position|
+      positions.each_with_object([]) do |position, braille_chars|
         braille_chars << [position.slice!(0..1)]
       end
-    end
-    braille_chars.join
+    end.join
+    # braille_chars.join
   end
 
   def separate_braille_chars(input)
