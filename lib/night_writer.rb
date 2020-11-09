85b53445ed04b10b3c8701de5a4f811f3699176a
Cydnee Owens <cks410@yahoo.com>
2020-11-08 20:12:48 -0700

    change require to english trans

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,20 +1,4 @@
-require './lib/dictionary'
-
-# handle        = File.open(ARGV[0], "r")
-# message       = handle.read.downcase.chomp
-# handle.close
-
-# char_limit    = message.scan((/.{1,40}/))
-
-# dictionary    = Dictionary.new
-
-# final_message = char_limit.map {|line| dictionary.translator(line)}.join("\n")
-
-# writer        = File.open(ARGV[1], "w")
-# writer.write(final_message)
-# writer.close
-
-# puts "Created #{ARGV[1]} containing #{message.length} characters"
+require './lib/english_translator'
 
 handle        = File.open(ARGV[0], "r")
 message       = handle.read.downcase.chomp
