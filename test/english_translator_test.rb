427908e32623d87742c2967caab315f3763f7f62
Cydnee Owens <cks410@yahoo.com>
2020-11-08 19:16:58 -0700

    add translate letter test

--- test/english_translator_test.rb
+++ test/english_translator_test.rb
@@ -11,4 +11,12 @@ class EnglishTranslatorTest < Minitest::Test
   def test_it_exists_and_has_attributes
     assert_instance_of EnglishTranslator, @eng_translator
   end
+
+  def test_it_can_translate_one_letter_to_braille
+    assert_equal ['0.', '00', '..'], @dictionary.translate_letter('h')
+    assert_equal ["00", "..", "0."], @dictionary.translate_letter('m')
+    assert_equal ['0.', '..', '00'], @dictionary.translate_letter('u')
+  end
+
+
 end
