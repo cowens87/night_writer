084f16ed41ea1cbd8bc90b6a9f1da85cfdd4eaff
Cydnee Owens <cks410@yahoo.com>
2020-11-08 19:02:24 -0700

    changes

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -8,9 +8,10 @@ dictionary         = Dictionary.new
 text               = dictionary.braille_translator(braille_message)
 require 'pry'; binding.pry
 final_braille      = text.scan(/.{1,80}/)+"\n"
-braille_translator = dictionary.braille_translator(final_braille)
+
+# braille_translator = dictionary.braille_translator(final_braille)
 writer             = File.open(ARGV[1], "w")
-writer.write(braille_translator)
+writer.write(final_braille)
 writer.close
 
 # will need to count the translated file to get an accurate character count
