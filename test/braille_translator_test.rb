d8e9934b5082a0368ab8760e10bac7080068f34a
Cydnee Owens <cks410@yahoo.com>
2020-11-09 06:37:31 -0700

    update naming conventions

--- test/braille_translator_test.rb
+++ test/braille_translator_test.rb
@@ -4,50 +4,50 @@ require './lib/braille_translator'
 
 class BrailleTranslatorTest < Minitest::Test
   def setup
-    @braille_translator = BrailleTranslator.new
+    @braillator = BrailleTranslator.new
   end
 
   def test_it_exists_and_has_attributes
-    assert_instance_of BrailleTranslator, @braille_translator
+    assert_instance_of BrailleTranslator, @braillator
   end
 
   def test_it_the_alphabet_has_been_reversed
     expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
       "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
-    assert_equal expected, @braille_translator.braille_dictionary.values
+    assert_equal expected, @braillator.braille_dictionary.values
   end
 
   def test_it_can_translate_one_braille_character
-    assert_equal 'g', @braille_translator.translate_braille_char(['00', '00', '..'])
+    assert_equal 'g', @braillator.translate_braille_char(['00', '00', '..'])
   end
   
   def test_it_can_translate_multiple_characters
     input = [['00', '0.', '0.'], ['.0', '0.', '..'],['00', '00', '..']]
-    assert_equal ["p", "i", "g"], @braille_translator.translate_braille_word(input)
+    assert_equal ["p", "i", "g"], @braillator.translate_braille_word(input)
   end
 
   def test_it_can_count_number_of_letters
     input    = "00.000.0..000.00\n0.0.000...0.0..0\n0.....0.....0.00\n"
-    assert_equal 8, @braille_translator.num_of_letters(input)
+    assert_equal 8, @braillator.num_of_letters(input)
     input    = "0..0\n000.\n....\n"
-    assert_equal 2, @braille_translator.num_of_letters(input)
+    assert_equal 2, @braillator.num_of_letters(input)
   end
 
   def test_it_can_convert_braille_input_into_linear_string
     input    = "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n"
     expected = "0.00..0..0..0.0.0.0.0.0.0..00........000.00..00.0.000.0.0.0.00.0.."
-    assert_equal expected, @braille_translator.join_braille_characters(input)
+    assert_equal expected, @braillator.join_braille_characters(input)
   end
 
   def test_it_can_separate_braille_characters
     input    = "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n"
     expected = ["0.00..", "0..0..", "0.0.0.", "0.0.0.", "0..00.", "......", ".000.0", "0..00.", "0.000.", "0.0.0.", "00.0.."]
-    assert_equal expected, @braille_translator.separate_braille_chars(input)
+    assert_equal expected, @braillator.separate_braille_chars(input)
   end
 
   def test_it_can_find_braille_characters
     input    = "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n"
-    assert_equal ['0.00..'], @braille_translator.find_braille_character(input).first
+    assert_equal ['0.00..'], @braillator.find_braille_character(input).first
   end
 
   def test_it_can_join_braille_dictionary_keys
@@ -61,13 +61,13 @@ class BrailleTranslatorTest < Minitest::Test
                 ["0...00"]=>"u", ["0.0.00"]=>"v", [".000.0"]=>"w", ["00..00"]=>"x", 
                 ["00.000"]=>"y", ["0..000"]=>"z", ["......"]=>" "
               }
-    assert_equal expected, @braille_translator.join_braille_dictionary_keys(input)
+    assert_equal expected, @braillator.join_braille_dictionary_keys(input)
   end
 
   def test_it_can_translate_from_braille_to_english
     input    = "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n"
-    assert_equal 'hello world', @braille_translator.braille_translator(input)
+    assert_equal 'hello world', @braillator.braille_translator(input)
     input    = "0..0\n000.\n....\n"
-    assert_equal 'hi', @braille_translator.braille_translator(input)
+    assert_equal 'hi', @braillator.braille_translator(input)
   end
 end
