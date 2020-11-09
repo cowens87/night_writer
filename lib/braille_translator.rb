0d0961fba9d5678bd24cccac1080b85095afce59
Cydnee Owens <cks410@yahoo.com>
2020-11-09 07:26:43 -0700

    add test helper

--- lib/braille_translator.rb
+++ lib/braille_translator.rb
@@ -25,7 +25,7 @@ class BrailleTranslator
     braille_chars = []
     positions = input.split("\n")
     num_of_letters(input).times do
-      positions.flat_map do |position|
+      positions.each do |position|
         braille_chars << [position.slice!(0..1)]
       end
     end
