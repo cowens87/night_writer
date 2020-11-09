5c9422bb76d60ee85750ccad5154553befeed04d
Cydnee Owens <cks410@yahoo.com>
2020-11-08 18:09:14 -0700

    add argument to braille dictionary

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -86,13 +86,13 @@ class Dictionary
     end
   end
 
-  def braille_dictionary
+  def braille_dictionary(input)
     @alphabet.invert
   end
 
   def join_braille_dictionary_keys(input)
     combined = {}
-    braille_dictionary.each do |braille, letter|
+    braille_dictionary(input).each do |braille, letter|
       combined[[braille.join(" ").gsub(/\s+/, "")]] = letter
     end
     combined
