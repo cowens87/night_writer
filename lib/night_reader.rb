c62d21b614dc71fe7f2f6207612da05dd9d36b7f
Cydnee Owens <cks410@yahoo.com>
2020-11-09 15:04:49 -0700

    update spacing

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,13 +1,13 @@
 require './lib/braille_translator'
 
-handle             = File.open(ARGV[0], "r")
-braille_message    = handle.read.chomp
+handle          = File.open(ARGV[0], "r")
+braille_message = handle.read.chomp
 handle.close
 
-braillator         = BrailleTranslator.new
-english            = braillator.braille_translator(braille_message)
+braillator      = BrailleTranslator.new
+english         = braillator.braille_translator(braille_message)
 
-writer             = File.open(ARGV[1], "w")
+writer          = File.open(ARGV[1], "w")
 writer.write(english)
 writer.close
 
