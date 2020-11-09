c303520fb06f3491549db50e61829d85e5e6f42b
Cydnee Owens <cks410@yahoo.com>
2020-11-08 19:36:08 -0700

    add initial test braille

--- test/braille_translator_test.rb
+++ test/braille_translator_test.rb
@@ -2,13 +2,12 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/braille_translator'
 
-class Test < Minitest::Test
+class BrailleTranslatorTest < Minitest::Test
   def setup
-    braille_translator = BrailleTranslator.new
+    @braille_translator = BrailleTranslator.new
   end
 
   def test_it_exists_and_has_attributes
-    assert_instance_of ,
-    assert_equal ,
+    assert_instance_of BrailleTranslator, @braille_translator
   end
 end
