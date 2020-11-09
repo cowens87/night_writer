b16e143d8e78b04615e16baaf648f870bc7f9cf6
Cydnee Owens <cks410@yahoo.com>
2020-11-09 05:14:23 -0700

    clean up of nw

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,13 +1,12 @@
-require './lib/dictionary'
+require './lib/braille_translator'
 
 handle             = File.open(ARGV[0], "r")
 braille_message    = handle.read.chomp
 handle.close
 
-dictionary         = Dictionary.new
-text               = dictionary.braille_translator(braille_message)
-require 'pry'; binding.pry
-final_braille      = text.scan(/.{1,80}/)+"\n"
+braillator         = BrailleTranslator.new
+text               = braillator.braille_translator(braille_message)
+# final_braille      = text.scan(/.{1,80}/)+"\n"
 
 # braille_translator = dictionary.braille_translator(final_braille)
 writer             = File.open(ARGV[1], "w")
@@ -15,4 +14,4 @@ writer.write(final_braille)
 writer.close
 
 # will need to count the translated file to get an accurate character count
-puts "Created #{ARGV[1]} containing #{braille_message.length} characters"
+puts "Created #{ARGV[1]} containing #{text.length} characters"
