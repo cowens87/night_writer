0764fffc87d1f12abf3d8526828bbf72857214a4
Cydnee Owens <cks410@yahoo.com>
2020-11-09 07:38:21 -0700

    update indentation

--- test/english_translator_test.rb
+++ test/english_translator_test.rb
@@ -25,7 +25,7 @@ class EnglishTranslatorTest < Minitest::Test
     assert_equal expected, @englator.translate_word('dad')
   end
 
-    def test_it_can_break_each_letter_down_by_top_middle_and_bottom_index
+  def test_it_can_break_each_letter_down_by_top_middle_and_bottom_index
     expected = [["0.", ".0"], ["00", "0."], ["..", ".."]]
     assert_equal expected, @englator.braille_split_top_mid_bottom('hi')
     expected = [["00", "0."], [".0", ".0"], ["0.", "0."]]
