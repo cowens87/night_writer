d161b0a3300a9e4ff9ba1116966f0dba91b70e1e
Cydnee Owens <cks410@yahoo.com>
2020-11-09 06:35:01 -0700

    add join braille keys method

--- test/braille_translator_test.rb
+++ test/braille_translator_test.rb
@@ -52,8 +52,16 @@ class BrailleTranslatorTest < Minitest::Test
 
   def test_it_can_join_braille_dictionary_keys
     input    = "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n"
-    expected = ["0.00..", "0..0..", "0.0.0.", "0.0.0.", "0..00.", "......", ".000.0", "0..00.", "0.000.", "0.0.0.", "00.0.."]
-    assert_equal expected, @braille_translator.join_braille_keys(input)
+    expected = {
+                ["0....."]=>"a", ["0.0..."]=>"b", ["00...."]=>"c", ["00.0.."]=>"d", 
+                ["0..0.."]=>"e", ["000..."]=>"f", ["0000.."]=>"g", ["0.00.."]=>"h", 
+                [".00..."]=>"i", [".000.."]=>"j", ["0...0."]=>"k", ["0.0.0."]=>"l", 
+                ["00..0."]=>"m", ["00.00."]=>"n", ["0..00."]=>"o", ["000.0."]=>"p", 
+                ["00000."]=>"q", ["0.000."]=>"r", [".00.0."]=>"s", [".0000."]=>"t", 
+                ["0...00"]=>"u", ["0.0.00"]=>"v", [".000.0"]=>"w", ["00..00"]=>"x", 
+                ["00.000"]=>"y", ["0..000"]=>"z", ["......"]=>" "
+              }
+    assert_equal expected, @braille_translator.join_braille_dictionary_keys(input)
   end
 
   def test_it_can_translate_from_braille_to_english
