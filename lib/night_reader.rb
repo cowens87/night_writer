2974ad6855f5e034431ed93b19125636d9451b3f
Cydnee Owens <cks410@yahoo.com>
2020-11-09 06:23:09 -0700

    update file structure

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -5,13 +5,11 @@ braille_message    = handle.read.chomp
 handle.close
 
 braillator         = BrailleTranslator.new
-text               = braillator.braille_translator(braille_message)
-# final_braille      = text.scan(/.{1,80}/)+"\n"
+english            = braillator.braille_translator(braille_message)
 
-# braille_translator = dictionary.braille_translator(final_braille)
 writer             = File.open(ARGV[1], "w")
-writer.write(final_braille)
+writer.write(english)
 writer.close
 
 # will need to count the translated file to get an accurate character count
-puts "Created #{ARGV[1]} containing #{text.length} characters"
+puts "Created #{ARGV[1]} containing #{english.length} characters"
