9d41b68be34ab7817226a185e2984fcfcfb33624
Cydnee Owens <cks410@yahoo.com>
2020-11-08 19:19:56 -0700

    add translate letter method

--- lib/english_translator.rb
+++ lib/english_translator.rb
@@ -2,4 +2,10 @@ require './lib/translatable'
 
 class EnglishTranslator 
   include Translatable
+
+  def translate_letter(letter)
+    alphabet[letter].collect do |braille_char|
+      braille_char
+    end
+  end
 end
--- test/english_translator_test.rb
+++ test/english_translator_test.rb
@@ -13,9 +13,9 @@ class EnglishTranslatorTest < Minitest::Test
   end
 
   def test_it_can_translate_one_letter_to_braille
-    assert_equal ['0.', '00', '..'], @dictionary.translate_letter('h')
-    assert_equal ["00", "..", "0."], @dictionary.translate_letter('m')
-    assert_equal ['0.', '..', '00'], @dictionary.translate_letter('u')
+    assert_equal ['0.', '00', '..'], @eng_translator.translate_letter('h')
+    assert_equal ["00", "..", "0."], @eng_translator.translate_letter('m')
+    assert_equal ['0.', '..', '00'], @eng_translator.translate_letter('u')
   end
 
 
